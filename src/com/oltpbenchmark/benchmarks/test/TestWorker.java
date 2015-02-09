8971c6941d8337a21c5412d21c1f7e440172d947
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2015-02-09 18:19:36 +0100

    First draft for worker done(Untested)

--- src/com/oltpbenchmark/benchmarks/test/TestWorker.java
+++ src/com/oltpbenchmark/benchmarks/test/TestWorker.java
@@ -1,30 +1,31 @@
 package com.oltpbenchmark.benchmarks.voter;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import com.oltpbenchmark.api.Procedure.UserAbortException;
 import com.oltpbenchmark.api.TransactionType;
 import com.oltpbenchmark.api.Worker;
-import com.oltpbenchmark.benchmarks.voter.PhoneCallGenerator.PhoneCall;
-import com.oltpbenchmark.benchmarks.voter.procedures.Vote;
+import com.oltpbenchmark.benchmarks.test.procedures.Move;
 import com.oltpbenchmark.types.TransactionStatus;
 
-public class VoterWorker extends Worker {
+public class TestWorker extends Worker {
 
-    private final PhoneCallGenerator switchboard;
-    
-    public VoterWorker(VoterBenchmark benchmarkModule, int id) {
+
+    public TestWorker(TestBenchmark benchmarkModule, int id) {
         super(benchmarkModule, id);
-        switchboard = new PhoneCallGenerator(0, benchmarkModule.numContestants);
     }
 
     @Override
     protected TransactionStatus executeWork(TransactionType txnType) throws UserAbortException, SQLException {
-        assert (txnType.getProcedureClass().equals(Vote.class));
-        PhoneCall call = switchboard.receive();
-        Vote proc = getProcedure(Vote.class);
+        assert (txnType.getProcedureClass().equals(Move.class));
+        Move proc = getProcedure(Move.class);
         assert (proc != null);
-        proc.run(conn, call.voteId, call.phoneNumber, call.contestantNumber, VoterConstants.MAX_VOTES);
+        Random rng = new Random();
+        int ship_id = rng.nextInt(TestConstants.NUM_SHIPS) + 1
+        int move_x = rng.nextInt(TestConstants.MAX_MOVE);
+        int move_y = rng.nextInt(TestConstants.MAX_MOVE);
+        proc.run(conn, ship_id, move_x, move_y);
         conn.commit();
         return TransactionStatus.SUCCESS;
     }
