d1c766f158fb75ee8fc90d72ae766812bb2a335e
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2015-02-11 00:09:06 +0100

    Removed old function

--- src/com/oltpbenchmark/benchmarks/test/TestUtil.java
+++ src/com/oltpbenchmark/benchmarks/test/TestUtil.java
@@ -1,18 +1,4 @@
 package com.oltpbenchmark.benchmarks.test;
 
 public class TestUtil {
-    /**
-     * Return the number of contestants to use for the given scale factor
-     * @param scaleFactor
-     */
-    public static int getScaledNumContestants(double scaleFactor) {
-        int min_contestants = 1;
-        int max_contestants = TestConstants.CONTESTANT_NAMES_CSV.split(",").length;
-
-        int num_contestants = (int)Math.round(TestConstants.NUM_CONTESTANTS * scaleFactor);
-        if (num_contestants < min_contestants) num_contestants = min_contestants;
-        if (num_contestants > max_contestants) num_contestants = max_contestants;
-
-        return (num_contestants);
-    }
 }
