aaa205534cf6b6032660ceef438bd814fd57b2a0
Carl Johnsen <wienie_master@yahoo.dk>
2015-04-05 00:58:59 +0200

    Created new branch, and added new SQL ddl's

--- src/com/oltpbenchmark/benchmarks/galaxy/galaxy-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/galaxy/galaxy-ddl.sql
@@ -1,44 +1,69 @@
 -- Drop tables before creating them
+DROP TABLE IF EXISTS fittings;
+DROP TABLE IF EXISTS fitting;
 DROP TABLE IF EXISTS ships;
 DROP TABLE IF EXISTS classes;
-DROP TABLE IF EXISTS solarsystems;
+DROP TABLE IF EXISTS solar_systems;
 
--- classes table holds all the ship classes and their reachability
+-- classes table holds all the ship classes and class related information
 CREATE TABLE classes
 (
-  cid integer           NOT NULL,
-  class varchar(40)     NOT NULL,
-  reachability integer  NOT NULL,
+  class_id            integer     NOT NULL,
+  class_name          varchar(40) NOT NULL,
+  reachability        integer     NOT NULL,
+  base_health_points  integer     NOT NULL,
+  max_fittings_count  integer     NOT NULL,
   PRIMARY KEY
   (
-    cid
+    class_id
   )
 );
 
--- solarsystems table holds all solarsystems and their size
-CREATE TABLE solarsystems
+-- solar_systems table holds all solar systems, their size and security level
+CREATE TABLE solar_systems
 (
-  ssid integer NOT NULL,
-  x_max integer NOT NULL,
-  y_max integer NOT NULL,
+  solar_system_id integer NOT NULL,
+  max_position_x  integer NOT NULL,
+  max_position_y  integer NOT NULL,
+  security_level  integer NOT NULL,
   PRIMARY KEY
   (
-    ssid
+    solar_system_id
   )
 );
 
--- ships table holds every ship, and their position
+-- ships table holds every ship, their position and current health points
 CREATE TABLE ships
 (
-  sid integer   NOT NULL,
-  x integer     NOT NULL,
-  y integer     NOT NULL,
-  class integer NOT NULL REFERENCES classes (cid),
-  ssid integer  NOT NULL REFERENCES solarsystems (ssid),
+  ship_id         integer NOT NULL,
+  position_x      integer NOT NULL,
+  position_y      integer NOT NULL,
+  class_id        integer NOT NULL REFERENCES classes (class_id),
+  solar_system_id integer NOT NULL REFERENCES solar_systems (solar_system_id),
+  health_points   integer NOT NULL,
   PRIMARY KEY
   (
-    sid
+    ship_id
   )
 );
-CREATE INDEX idx_ships_x ON ships (x);
-CREATE INDEX idx_ships_y ON ships (y);
+CREATE INDEX idx_ships_x ON ships (position_x);
+CREATE INDEX idx_ships_y ON ships (position_y);
+
+-- fitting table holds information about a fitting
+CREATE TABLE fitting
+(
+  fitting_id    integer NOT NULL,
+  fitting_type  integer NOT NULL,
+  fitting_value integer NOT NULL,
+  PRIMARY KEY
+  (
+    fitting_id
+  )
+);
+
+-- fittings table hold the links between ships and fittings
+CREATE TABLE fittings
+(
+  ship_id     integer NOT NULL REFERENCES ships (ship_id),
+  fitting_id  integer NOT NULL REFERENCES fitting (fitting_id),
+);
--- src/com/oltpbenchmark/benchmarks/galaxy/galaxy-hsqldb-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/galaxy/galaxy-hsqldb-ddl.sql
@@ -1,44 +1,69 @@
 -- Drop tables before creating them
+DROP TABLE IF EXISTS FITTINGS;
+DROP TABLE IF EXISTS FITTING;
 DROP TABLE IF EXISTS SHIPS;
 DROP TABLE IF EXISTS CLASSES;
-DROP TABLE IF EXISTS SOLARSYSTEMS;
+DROP TABLE IF EXISTS SOLAR_SYSTEMS;
 
--- classes table holds all the ship classes and their reachability
+-- classes table holds all the ship classes and class related information
 CREATE TABLE CLASSES
 (
-  cid integer           NOT NULL,
-  class varchar(40)     NOT NULL,
-  reachability integer  NOT NULL,
+  class_id            integer     NOT NULL,
+  class_name          varchar(40) NOT NULL,
+  reachability        integer     NOT NULL,
+  base_health_points  integer     NOT NULL,
+  max_fittings_count  integer     NOT NULL,
   PRIMARY KEY
   (
-    cid
+    class_id
   )
 );
 
--- solarsystems table holds all solarsystems and their size
-CREATE TABLE SOLARSYSTEMS
+-- solar_systems table holds all solar systems, their size and security level
+CREATE TABLE SOLAR_SYSTEMS
 (
-  ssid integer NOT NULL,
-  x_max integer NOT NULL,
-  y_max integer NOT NULL,
+  solar_system_id integer NOT NULL,
+  max_position_x  integer NOT NULL,
+  max_position_y  integer NOT NULL,
+  security_level  integer NOT NULL,
   PRIMARY KEY
   (
-    ssid
+    solar_system_id
   )
 );
 
--- ships table holds every ship, and their position
+-- ships table holds every ship, their position and current health points
 CREATE TABLE SHIPS
 (
-  sid integer   NOT NULL,
-  x integer     NOT NULL,
-  y integer     NOT NULL,
-  class integer NOT NULL REFERENCES CLASSES (cid),
-  ssid integer  NOT NULL REFERENCES solarsystems (ssid),
+  ship_id         integer NOT NULL,
+  position_x      integer NOT NULL,
+  position_y      integer NOT NULL,
+  class_id        integer NOT NULL REFERENCES CLASSES (class_id),
+  solar_system_id integer NOT NULL REFERENCES SOLAR_SYSTEMS (solar_system_id),
+  health_points   integer NOT NULL,
   PRIMARY KEY
   (
-    sid
+    ship_id
   )
 );
-CREATE INDEX idx_ships_x ON ships (x);
-CREATE INDEX idx_ships_y ON ships (y);
+CREATE INDEX idx_ships_x ON SHIPS (position_x);
+CREATE INDEX idx_ships_y ON SHIPS (position_y);
+
+-- fitting table holds information about a fitting
+CREATE TABLE FITTING
+(
+  fitting_id    integer NOT NULL,
+  fitting_type  integer NOT NULL,
+  fitting_value integer NOT NULL,
+  PRIMARY KEY
+  (
+    fitting_id
+  )
+);
+
+-- fittings table hold the links between ships and fittings
+CREATE TABLE FITTINGS
+(
+  ship_id     integer NOT NULL REFERENCES SHIPS (ship_id),
+  fitting_id  integer NOT NULL REFERENCES FITTING (fitting_id),
+);
