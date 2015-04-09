7d669ead52da04cd4cb0356c9e07f9018d713fbc
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2015-04-09 12:52:49 +0200

    More work on combat procedure, and the Ship class

--- src/com/oltpbenchmark/benchmarks/galaxy/procedures/Combat.java
+++ src/com/oltpbenchmark/benchmarks/galaxy/procedures/Combat.java
@@ -11,6 +11,7 @@ import java.lang.Math;
 import com.oltpbenchmark.api.Procedure;
 import com.oltpbenchmark.api.SQLStmt;
 import com.oltpbenchmark.benchmarks.galaxy.GalaxyConstants;
+import com.oltpbenchmark.benchmarks.galaxy.util.Ship;
 import com.oltpbenchmark.util.Pair;
 
 /**
@@ -25,9 +26,14 @@ public class Combat extends Procedure {
 
     // Get all ships within a range in a specified solarsystem
     public final SQLStmt queueRangeShips = new SQLStmt(
-        "SELECT * FROM " + GalaxyConstants.TABLENAME_SHIPS +
+        "SELECT ship_id, health_points FROM " + GalaxyConstants.TABLENAME_SHIPS +
         " WHERE position_x BETWEEN ? AND ? AND position_y BETWEEN ? AND ? AND " +
         "solar_system_id = ?;"
+    );
+
+    public final SQLStmt getShipFittings = new SQLStmt(
+        "SELECT SUM(fitting_value) FROM " + GalaxyConstants.TABLENAME_FITTINGS +
+        " WHERE ship_id = ? AND fitting_type = ?;"
     );
 
     /**
@@ -37,6 +43,18 @@ public class Combat extends Procedure {
      */
     public long run(Connection conn, int solarSystemId, Pair<Integer, Integer> minPos,
         Pair<Integer, Integer> maxPos, Random rng) throws SQLException {
+        ArrayList<Ship> ships = getShipInformation(conn, solarSystemId, minPos, maxPos);
+        ships = getShipDamageDefence(conn, ships);
+
+        for (int i = 0; i < ships.size(); i++) {
+            if (i % 2 == 0) {
+        }
+
+        return COMBAT_SUCCESSFUL
+    }
+
+    private ArrayList<Ship> getShipInformation(conn, solarSystemId, Pair<Integer, Integer> minPos,
+        Pair<Integer, Integer> maxPos) throws SQLException {
         // Get ship information
         PreparedStatement ps = getPreparedStatement(conn, queueRangeShips);
         ps.setInt(1, minPos.first);
@@ -45,95 +63,55 @@ public class Combat extends Procedure {
         ps.setInt(4, maxPos.second);
         ps.setInt(5, solarSystemId);
         ResultSet rs = ps.executeQuery();
-        Pair<Integer, Integer> position;
-        int reachability;
-        int ssid;
-        Pair<Integer, Integer> systemMax;
+        ArrayList<Ship> ships = new ArrayList<Ship>;
+        int shipId;
+        int healthPoints;
         try {
             if (!rs.next()) {
-                System.out.println("Ship: " + Integer.toString(shipId));
-                return ERR_INVALID_SHIP;
+                throw new SQLException();
             } else {
-                position = new Pair<Integer, Integer>
-                        (rs.getInt(1), rs.getInt(2));
-                reachability = rs.getInt(3);
-                ssid = rs.getInt(4);
-                systemMax = new Pair<Integer, Integer>
-                        (rs.getInt(5), rs.getInt(6));
+                shipId = rs.getInt(1);
+                healthPoints = rs.getInt(2);
+                ships.add(new Ship(shipId, healthPoints);
             }
         } finally {
             rs.close();
         }
-
-        // Cap the movement to reachability
-        if (move_x < 0) {
-            move_x = Math.max(move_x, -reachability);
-        } else {
-            move_x = Math.min(move_x, reachability);
-        }
-        if (move_y < 0) {
-            move_y = Math.max(move_y, -reachability);
-        } else {
-            move_y = Math.min(move_y, reachability);
-        }
-        Pair<Integer, Integer> newPosition = new Pair<Integer, Integer>
-                (Math.max(Math.min(systemMax.first, position.first + move_x), 0),
-                 Math.max(Math.min(systemMax.second, position.second + move_y), 0));
-
-        // Prepare the list of possible positions
-        ArrayList<Pair<Integer, Integer>> possibles =
-                new ArrayList<Pair<Integer, Integer>>();
-        Pair<Integer, Integer> min = new Pair<Integer, Integer>
-                (Math.max(0, newPosition.first - 1),
-                 Math.max(0, newPosition.second));
-        Pair<Integer, Integer> max = new Pair<Integer, Integer>
-                (Math.min(systemMax.first, newPosition.first + 1),
-                 Math.min(systemMax.second, newPosition.second + 1));
-        for (int i = min.first; i <= max.first; i++) {
-            for (int j = min.second; j <= max.second; j++) {
-                possibles.add(new Pair<Integer, Integer>(i,j));
-            }
-        }
-
-        // Get all ships in the window
-        // and remove them from the possible positions
-        ps = getPreparedStatement(conn, checkTileStmt);
-        ps.setInt(1, newPosition.first);
-        ps.setInt(2, newPosition.first);
-        ps.setInt(3, newPosition.second);
-        ps.setInt(4, newPosition.second);
-        ps.setInt(5, ssid);
-        ps.setInt(6, shipId);
-        rs = ps.executeQuery();
-        try {
-            while (rs.next()) {
-                Pair<Integer, Integer> taken = new Pair<Integer, Integer>
-                        (rs.getInt(1), rs.getInt(2));
-                for (int i = 0; i < possibles.size(); i++) {
-                    if (taken.equals(possibles.get(i))) {
-                        possibles.remove(i);
-                    }
-                }
-            }
-        } finally {
-            rs.close();
-        }
-
-        // Select a random position among the possibles, if any
-        if (possibles.size() == 0) {
-            return MOVE_NOT_SUCCESSFUL;
-        } else {
-            newPosition = possibles.get(rng.nextInt(possibles.size()));
-        }
-
-        // Update the ships position
-        ps = getPreparedStatement(conn, updateShipPosStmt);
-        ps.setInt(1, newPosition.first);
-        ps.setInt(2, newPosition.second);
-        ps.setInt(3, shipId);
-        ps.execute();
-
-        // Set the return value to 0: successful move
-        return MOVE_SUCCESSFUL;
+        return ships;
     }
+
+    private ArrayList<Ship> getShipDamageDefence(conn, ArrayList<Ship>) throws SQLException {
+        int tempDamage;
+        int tempDefence;
+        ResultSet rs;
+        for (Ship ship : ships) {
+            ps = getPreparedStatement(conn, getShipFittings);
+            ps.setInt(1, ship.shipId);
+            ps.setInt(2, GalaxyConstants.FITTING_TYPE_OFFENSIVE);
+            rs = ps.executeQuery();
+            try {
+                if (!rs.next()) {
+                    throw new SQLException();
+                } else {
+                    tempDamage = rs.getInt(1);
+                }
+            } finally {
+                rs.close();
+            }
+            ps.setInt(2, GalaxyConstants.FITTING_TYPE_DEFENSIVE);
+            rs = ps.executeQuery();
+            try {
+                if (!rs.next()) {
+                    throw new SQLException();
+                } else {
+                    tempDefence = rs.getInt(2);
+                }
+            } finally {
+              rs.close();
+            }
+            ship.setDamageDefence(tempDamage, tempDefence);
+        }
+        return ships;
+    }
+
 }
--- src/com/oltpbenchmark/benchmarks/galaxy/util/Ship.java
+++ src/com/oltpbenchmark/benchmarks/galaxy/util/Ship.java
@@ -5,46 +5,20 @@ import java.util.ArrayList;
 import com.oltpbenchmark.util.Pair;
 
 public class Ship {
-    private final int shipId;
-    private final int pos_x;
-    private final int pos_y;
-    private final int classId;
-    private final int solarSystemId;
-    private final int healthPoints;
+    protected final int shipId;
+    protected final int healthPoints;
 
-    private final int damage;
-    private final int defence;
+    protected final int damage;
+    protected final int defence;
 
-    public Ship(int shipId, int pos_x, int pos_y, int classId,
-        int solarSystemId, int healthPoints) {
-      this.shipId = shipId;
-      this.pos_x = pos_x;
-      this.pos_y = pos_y;
-      this.classId = classId;
-      this.solarSystemId = solarSystemId;
-      this.healthPoints = healthPoints;
+    public Ship(int shipId, int healthPoints) {
+        this.shipId = shipId;
+        this.healthPoints = healthPoints;
     }
 
-    public void statsFromFittings(ArrayList<Pair<int, bool>> fittingValues) {
-        int tempDamage = 0;
-        int tempDefence = 0;
-        for (Pair<int, bool> valueType : fittingValues) {
-            if (valueType.second) {
-                tempDamage += Pair.first;
-            } else {
-                tempDefence += Pair.first;
-            }
-        }
-        this.damage = tempDamage;
-        this.defence = tempDefence;
-    }
-
-    public int getDamage() {
-        return this.damage
-    }
-
-    public int getDefence() {
-        return this.defence
+    public void setDamageDefence(int damage, int defence) {
+        this.damage = damage;
+        this.defence = defence;
     }
 
 }
