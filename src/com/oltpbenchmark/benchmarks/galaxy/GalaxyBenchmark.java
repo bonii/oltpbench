94cddd51820e91a508e358d9da1fc8f7a3c30b34
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2015-05-05 23:28:46 +0200

    Work done on generating activity regions

--- src/com/oltpbenchmark/benchmarks/galaxy/GalaxyBenchmark.java
+++ src/com/oltpbenchmark/benchmarks/galaxy/GalaxyBenchmark.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.lang.Math;
 
 import com.oltpbenchmark.WorkloadConfiguration;
 import com.oltpbenchmark.api.BenchmarkModule;
@@ -45,5 +47,58 @@ public class GalaxyBenchmark extends BenchmarkModule {
     protected Package getProcedurePackageImpl() {
        return Move.class.getPackage();
     }
+
+    private final SQLStmt querySolarSystems= "SELECT * FROM " +
+        GalaxyConstants.TABLENAME_SOLARSYSTEMS + ";";
+
+
+    private ArrayList<ActivityRegion> generateActivityRegions(int numWorkers) {
+        PreparedStatement ps = getPreparedStatement(conn, querySolarSystems);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<SolarSystem> solarSystems = new ArrayList<SolarSystem>();
+        ArrayList<ActivityRegion> regions = new ArrayList<ActivityRegion>();
+
+        try {
+            while (rs.next()) {
+                SolarSystem solar = new SolarSystem(rs.getInt(1), rs.getInt(5), rs.getLong(2),
+                        rs.getLong(3), rs.getLong(4));
+                solarSystems.add(solar);
+            }
+        } finally {
+            rs.close();
+        }
+        int regionsPerSolarSystems = 2 * numWorkers / solarSystems.size();
+        for (solar : solarSystems) {
+            regions.addAll(getSolarRegions(regionsPerSolarSystem, solar);
+        }
+
+    }
+
+    private getSolarRegions(int numRegions, SolarSystem solar) {
+        ArrayList<ActivityRegion> regions = new ArrayList<ActivityRegion>();
+        long sizeX = solar.xMax / Math.max(1, numRegions / 2);
+        long sizeY = solar.yMax / Math.max(1, numRegions / 2);
+        long sizeZ = solar.zMax / Math.max(1, numRegions / 2);
+        Random rng = new Random();
+        for (int i = 0; i < numRegions, i++) {
+            xPos = rng.nextInt(solar.xMax - sizeX);
+            yPos = rng.nextInt(solar.yMax - sizeY);
+            zPos = rng.nextInt(solar.zMax - sizeZ);
+
+            ImmutableTriple<long, long, long> minPos =
+                new ImmutableTriple<long, long, long> (xPos, yPos, zPos);
+            ImmutableTriple<long, long, long> maxPos =
+                new ImmutableTriple<long, long, long> (xPos + sizeX, yPos + sizeY, zPos + sizeZ);
+            ArrayList<int> probabilityVector = getProbabilityVector(minPos, maxPos, solar.securityLevel);
+
+            regions.add(new ActivityRegion(solar.solarSystemId, minPos, maxPos, probabilityVector));
+        }
+
+        return regions;
+    }
+
+    private getProbabilityVector(ImmutableTriple<long, long, long> minPos,
+            ImmutableTriple<long, long, long> maxPos, int securityLevel) {
+
 
 }
