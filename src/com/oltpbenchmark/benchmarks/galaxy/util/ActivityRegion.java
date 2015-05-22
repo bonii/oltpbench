3993936f5b160e904efcb46684c63754f2a891b2
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2015-05-23 00:27:20 +0200

    Removed the probabilityVector property, not used.

--- src/com/oltpbenchmark/benchmarks/galaxy/util/ActivityRegion.java
+++ src/com/oltpbenchmark/benchmarks/galaxy/util/ActivityRegion.java
@@ -7,16 +7,14 @@ public class ActivityRegion {
     public final int solarSystemId;
     public final ImmutableTriple<Long, Long, Long> minPos;
     public final ImmutableTriple<Long, Long, Long> maxPos;
-    public final HashMap<String, Integer> probabilityVector;
 
     public ActivityRegion(int solarSystemId, ImmutableTriple<Long, Long, Long> minPos,
-            ImmutableTriple<Long, Long, Long> maxPos, HashMap<String, Integer> probabilityVector) {
+            ImmutableTriple<Long, Long, Long> maxPos) {
         this.solarSystemId  = solarSystemId;
         this.minPos       = minPos;
         this.maxPos         = maxPos;
-        this.probabilityVector = probabilityVector;
     }
-    
+
     public String toString() {
         String result = "" + solarSystemId + ",";
         result += "" + minPos.left + "," + minPos.middle + "," + minPos.right + ",";
