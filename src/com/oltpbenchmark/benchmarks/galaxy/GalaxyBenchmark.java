67960cc78ced44e0adf3c2c726ccc91f9b657ff9
carljohnsen <wienie_master@yahoo.dk>
2015-06-02 23:18:54 +0200

    Fixed error when loading and executing, by making and closing connection

--- src/com/oltpbenchmark/benchmarks/galaxy/GalaxyBenchmark.java
+++ src/com/oltpbenchmark/benchmarks/galaxy/GalaxyBenchmark.java
@@ -71,14 +71,6 @@ public class GalaxyBenchmark extends BenchmarkModule {
         GalaxyConstants.TABLENAME_SOLARSYSTEMS + ";"
         ;
 
-    private final String queryShips = "SELECT COUNT(*) FROM " +
-        GalaxyConstants.TABLENAME_SHIPS + " WHERE " +
-        "position_x BETWEEN ? AND ? AND " +
-        "position_y BETWEEN ? AND ? AND " +
-        "position_z BETWEEN ? AND ? AND " +
-        "solar_system_id = ?;";
-
-
     /**
      * Generates ActivityRegions for the benchmark
      * @param numWorkers number of workers in use
@@ -87,8 +79,7 @@ public class GalaxyBenchmark extends BenchmarkModule {
      */
     private ArrayList<ActivityRegion> generateActivityRegions(int numWorkers)
             throws SQLException {
-        Connection conn = getLastConnection();
-        if (conn == null) conn = makeConnection();
+        Connection conn = makeConnection();
         PreparedStatement ps = conn.prepareStatement(querySolarSystems);
         ResultSet rs = ps.executeQuery();
         ArrayList<SolarSystem> solarSystems = new ArrayList<SolarSystem>();
@@ -103,10 +94,11 @@ public class GalaxyBenchmark extends BenchmarkModule {
         } finally {
             rs.close();
         }
+        conn.close();
         // int regionsPerSolarSystem = Math.max(10, 20 * numWorkers / solarSystems.size());
-            int regionsPerSolarSystem = 20;
+        int regionsPerSolarSystem = 20;
         for (SolarSystem solar : solarSystems) {
-            regions.addAll(getSolarRegions(regionsPerSolarSystem, conn, solar));
+            regions.addAll(getSolarRegions(regionsPerSolarSystem, solar));
         }
         return regions;
     }
@@ -116,7 +108,7 @@ public class GalaxyBenchmark extends BenchmarkModule {
      * @param numRegions number of regions for the solar system
      * @param solar the SolarSystem object for the solar system
      */
-    private ArrayList<ActivityRegion> getSolarRegions(int numRegions, Connection conn, SolarSystem solar)
+    private ArrayList<ActivityRegion> getSolarRegions(int numRegions, SolarSystem solar)
             throws SQLException {
         ArrayList<ActivityRegion> regions = new ArrayList<ActivityRegion>();
         double xMax = solar.xMax.floatValue();
@@ -138,28 +130,7 @@ public class GalaxyBenchmark extends BenchmarkModule {
             ImmutableTriple<Long, Long, Long> maxPos =
                 new ImmutableTriple<Long, Long, Long> (xPos + sizeX, yPos + sizeY, zPos + sizeZ);
 
-            int count = 0;
-            PreparedStatement ps = conn.prepareStatement(queryShips);
-            ps.setLong(1, minPos.left);
-            ps.setLong(2, maxPos.left);
-            ps.setLong(3, minPos.middle);
-            ps.setLong(4, maxPos.middle);
-            ps.setLong(5, minPos.right);
-            ps.setLong(6, maxPos.right);
-            ps.setInt(7, solar.solarSystemId);
-            ResultSet rs = ps.executeQuery();
-            try {
-                if (rs.next())
-                    count = rs.getInt(1);
-                else
-                    throw new SQLException();
-            } finally {
-                rs.close();
-            }
-            if (count <= 1)
-                i--;
-            else
-                regions.add(new ActivityRegion(solar.solarSystemId, minPos, maxPos));
+            regions.add(new ActivityRegion(solar.solarSystemId, minPos, maxPos));
         }
 
         return regions;
